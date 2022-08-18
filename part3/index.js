9145f2925e13db4e0d896caa26703ec0f059e4ae
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2022-08-18 11:15:35 -0500

    commit con heroku config

--- part3/index.js
+++ part3/index.js
@@ -76,5 +76,5 @@ app.post('/api/notes', (request, response) => {
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port... ${PORT}`)
 })
